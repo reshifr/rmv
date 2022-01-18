8c405aa06cfbfccde8bf8bfb395cb79646c447a9
Renol P. H <reshifr@gmail.com>
2022-01-18 13:36:59 +0700

    fix: running code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-#define RMV_DEBUG
+#define RMV_DEBUG_
 
-#include <cstdio>
 #include <chrono>
 #include <random>
 #include <string>
@@ -14,58 +13,67 @@
 # include <execution>
 #endif
 
+namespace rmvmain {
+
 using namespace std;
 using namespace chrono;
 
-class timer {
+class clock {
   private:
+    decltype(high_resolution_clock::now()) a, b;
+  
+  public:
     mt19937_64 rng;
-    decltype(high_resolution_clock::now()) x, y;
+    random_device dev;
 
   public:
-    timer(void) :
-      rng(high_resolution_clock::now().time_since_epoch().count()) {}
-    void start(void) { x = high_resolution_clock::now(); }
+    clock(void) { rng = mt19937_64(dev()); }
+    void start(void) { a = high_resolution_clock::now(); }
     
-    template <class P>
-    int64_t result(void) {
-      y = high_resolution_clock::now();
-      return duration_cast<P>(y-x).count();
+    template<class P>
+    decltype(auto) result(void) {
+      b = high_resolution_clock::now();
+      return duration_cast<P>(b-a).count();
     }
 
-    template <class T>
-    T random(T l, T h) {
-      static_assert(is_scalar<T>::value, "Error: Use scalar type!\n");
-      uniform_int_distribution<T> rand(l, h);
-      return rand(rng);
+    template<class T>
+    T random(T lo, T hi) {
+      uniform_int_distribution<T> rd(lo, hi);
+      return rd(rng);
     }
 };
 
-#define MILLION(_n) (_n##000##000##ULL)
-#define M MILLION(1)
-#define N 333
-#define E 3
+#define Mil(_n) (_n##000##000)
+#define N Mil(1)
+#define E 4
 
-int main(void) {
-  timer clock;
-  rsfr::rcmv<E, int> mv(N);
+void run(void) {
+  clock cl;
+  rsfr::rpmv<E, int> mv(10);
   for(auto& elm : mv)
-    elm = clock.random(1, 99);
+    elm = cl.random(1, 99);
 
   int x = 300;
   mv.push_back(x);
   mv.push_back(333);
 
-  clock.start();
+  cl.start();
 #if defined(__GNUG__) && defined(_OPENMP)
   __gnu_parallel::stable_sort(mv.rbegin(), mv.rend());
 #elif defined(_MSC_VER)
   stable_sort(execution::par, mv.rbegin(), mv.rend());
 #endif
-  cout<<"clock = "<<clock.result<milliseconds>()<<" ms"<<endl<<endl;
+  cout<<"clock = "<<cl.result<milliseconds>()<<" ms"<<endl<<endl;
+  mv.debug();
 
   for(auto& elm : mv)
     cout<<elm<<" ";
   cout<<endl<<endl;
-  mv.debug();
+}
+
+}
+
+int main(void) {
+  rmvmain::run();
+  return EXIT_SUCCESS;
 }
